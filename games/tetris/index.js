4b727d674803f78e66d5a30539d1cb206c889d86
PetarIvancevic <petar.ivancevic171@gmail.com>
2017-10-23 20:30:46 +0200

    fixed multiple clearance bug

--- games/tetris/index.js
+++ games/tetris/index.js
@@ -1,21 +1,75 @@
-import _ from 'lodash'
+import _size from 'lodash/size'
+import _keys from 'lodash/keys'
+import _random from 'lodash/random'
 import gameBlocks from './components'
 import {games} from 'constants'
 
 const game = function (difficulty) {
-  const gameBoard = new Array(10)
+  let gameBoard = new Array(10)
   let gameOver = false
   let score = 0
   let frame = 1
   let currentBlock, currentBlockType, nextBlockType
 
-  const possibleBlockTypes = _.keys(games.tetris.blockTypes)
+  const possibleBlockTypes = _keys(games.tetris.blockTypes)
+
+  const advanceCurrentBlock = function () {
+    if (frame % difficulty === 0) {
+      currentBlock.advance(checkCollision)
+    }
+    frame++
+  }
 
   this.advanceGame = function () {
     if (gameOver) {
       return
     }
     advanceCurrentBlock()
+  }
+
+  const createBlock = function () {
+    currentBlock = new gameBlocks[currentBlockType](fixateBlockAndSetNewBlock, isRotationPossible)
+
+    for (let i = 0; i < _size(currentBlock.occupiedPositions); i++) {
+      let {x, y} = currentBlock.occupiedPositions[i]
+
+      if (checkCollision(x, y)) {
+        return endGame()
+      }
+    }
+  }
+
+  const createGameBoard = function () {
+    for (let i = 0; i < 10; i++) {
+      gameBoard[i] = new Array(20)
+    }
+  }
+
+  const checkCollision = function (x, y) {
+    return !!gameBoard[x][y]
+  }
+
+  const endGame = function () {
+    gameOver = true
+    document.body.removeEventListener('keypress', registerEventListeners)
+    console.log('endGame')
+  }
+
+  const fixateBlockAndSetNewBlock = function (type, occupiedPositions) {
+    for (let i = 0; i < _size(occupiedPositions); i++) {
+      let {x, y} = occupiedPositions[i]
+
+      gameBoard[x][y] = {type}
+    }
+
+    setupNextBlock()
+    const fullRowCount = getFullRowsCount()
+    createBlock()
+
+    if (fullRowCount) {
+      score += fullRowCount * 10
+      pushFullRowsDown()
+    }
   }
 
   this.getBoard = function () { return gameBoard }
@@ -26,25 +80,8 @@ const game = function (difficulty) {
 
   this.getCurrentBlock = function () { return currentBlock }
 
-  const createGameBoard = function () {
-    for (let i = 0; i < 10; i++) {
-      gameBoard[i] = new Array(20)
-    }
-  }
-
-  const advanceCurrentBlock = function () {
-    if (frame % difficulty === 0) {
-      currentBlock.advance(checkCollision)
-    }
-    frame++
-  }
-
-  const checkCollision = function (x, y) {
-    return !!gameBoard[x][y]
-  }
-
   const isRotationPossible = function (positions) {
-    for (let i = 0; i < _.size(positions); i++) {
+    for (let i = 0; i < _size(positions); i++) {
       let {x, y} = positions[i]
 
       if (positions[i].x < 0 || positions[i].x > 9 || positions[i].y < 0 || positions[i].y > 19) {
@@ -59,14 +96,8 @@ const game = function (difficulty) {
     return true
   }
 
-  const endGame = function () {
-    gameOver = true
-    document.body.removeEventListener('keypress', registerEventListeners)
-    console.log('endGame')
-  }
-
   const getRandomBlockType = function () {
-    return possibleBlockTypes[_.random(0, _.size(possibleBlockTypes) - 1)]
+    return possibleBlockTypes[_random(0, _size(possibleBlockTypes) - 1)]
   }
 
   const setupNextBlock = function () {
@@ -74,61 +105,67 @@ const game = function (difficulty) {
     nextBlockType = getRandomBlockType()
   }
 
-  const pushDownFromHeight = function (height) {
+  const pushFullRowsDown = function () {
+    function pushRowsDownFromIndex (rowIndex) {
+      for (let i = 0; i < 10; i++) {
+        delete tempGameBoard[i][rowIndex]
+      }
+
+      for (let i = rowIndex; i > 0; i--) {
+        for (let j = 0; j < 10; j++) {
+          tempGameBoard[j][i] = tempGameBoard[j][i - 1]
+        }
+      }
+
+      for (let i = 0; i < 10; i++) {
+        tempGameBoard[i][0] = null
+      }
+    }
+
+    let tempGameBoard = []
+
     for (let i = 0; i < 10; i++) {
-      for (let j = height; j >= 0; j--) {
-        if (j === 0) {
-          delete gameBoard[i][j]
-        } else {
-          gameBoard[i][j] = gameBoard[i][j - 1]
+      tempGameBoard[i] = gameBoard[i].slice()
+    }
+
+    while (getFullRowsCount(tempGameBoard)) {
+      for (let i = 19; i >= 0; i--) {
+        let isRowFull = true
+
+        for (let j = 0; j < 10; j++) {
+          if (!tempGameBoard[j][i]) {
+            isRowFull = false
+          }
+        }
+
+        if (isRowFull) {
+          pushRowsDownFromIndex(i)
+          i++
         }
       }
     }
+
+    gameBoard = tempGameBoard
   }
 
-  const checkForFullRows = function () {
+  const getFullRowsCount = function (tempGameBoard = gameBoard) {
     let numberOfFullRows = 0
 
     for (let i = 19; i >= 0; i--) {
       let isRowFull = true
 
       for (let j = 0; j < 10; j++) {
-        if (!gameBoard[j][i]) {
+        if (!tempGameBoard[j][i]) {
           isRowFull = false
         }
       }
 
       if (isRowFull) {
         numberOfFullRows++
-        pushDownFromHeight(i)
       }
     }
 
-    score += numberOfFullRows
-  }
-
-  const fixateBlockAndGetNew = function (type, occupiedPositions) {
-    for (let i = 0; i < _.size(occupiedPositions); i++) {
-      let {x, y} = occupiedPositions[i]
-
-      gameBoard[x][y] = {type}
-    }
-
-    setupNextBlock()
-    checkForFullRows()
-    createBlock()
-
-    for (let i = 0; i < _.size(currentBlock.occupiedPositions); i++) {
-      let {x, y} = currentBlock.occupiedPositions[i]
-
-      if (checkCollision(x, y)) {
-        return endGame()
-      }
-    }
-  }
-
-  const createBlock = function () {
-    currentBlock = new gameBlocks[currentBlockType](fixateBlockAndGetNew, isRotationPossible)
+    return numberOfFullRows
   }
 
   const registerEventListeners = function (event) {
