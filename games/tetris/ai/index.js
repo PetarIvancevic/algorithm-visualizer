a5627ad7839892ed6dc35e012b18d066d19ed1d8
PetarIvancevic <petar.ivancevic171@gmail.com>
2018-04-07 00:14:52 +0200

    added reward calculation

--- games/tetris/ai/index.js
+++ games/tetris/ai/index.js
@@ -12,6 +12,14 @@ const TreeNode = function (parentNode, currentBlock) {
   }
 }
 
+/*
+  Make move with block
+  - left
+  - right
+  - down (to lowest point bottom)
+  - rotate
+*/
+
 function makeMove (currentBlock, move, checkCollisionFn) {
   if (move === 'right' || move === 'left') {
     currentBlock.move(move)
@@ -25,6 +33,10 @@ function makeMove (currentBlock, move, checkCollisionFn) {
 
   return currentBlock
 }
+
+/*
+  Generate all possible moves
+*/
 
 function generateMoves (currentBlock, checkCollisionFn) {
   if (!currentBlock.isMovable) {
@@ -61,6 +73,79 @@ function getParentNode (parentMove, allMoveNodes) {
   })
 }
 
+function addYCoord (occupiedYPositions) {
+  if ((_.max(occupiedYPositions) + 1) <= 19) {
+    occupiedYPositions.push(_.max(occupiedYPositions) + 1)
+  } else {
+    occupiedYPositions.push(_.min(occupiedYPositions) - 1)
+  }
+}
+
+function populateToFourYCoords (occupiedPositions) {
+  let occupiedYPositions = _(occupiedPositions).map('y').union().value()
+  const numYCoords = _.size(occupiedYPositions)
+
+  if (numYCoords !== 4) {
+    for (let i = 0; i < (4 - numYCoords); i++) {
+      addYCoord(occupiedYPositions)
+    }
+  }
+
+  return occupiedYPositions
+}
+
+function getFullRowCount (board, occupiedYPositions) {
+  let fullRowCount = 0
+
+  for (let i = 0; i < 4; i++) {
+    let currentRow = occupiedYPositions[i]
+    let rowIsFull = true
+
+    for (let column = 0; column < 10; column++) {
+      if (!board[column][currentRow]) {
+        rowIsFull = false
+        break
+      }
+    }
+
+    if (rowIsFull) {
+      fullRowCount++
+    }
+  }
+
+  return fullRowCount
+}
+
+function calculateReward (board, occupiedYPositions, minialYIndex) {
+  if (minialYIndex < 4) {
+    return -100000
+  }
+
+  const fullRowCount = getFullRowCount(board, occupiedYPositions)
+  const bonusPoints = fullRowCount > 1 ? fullRowCount * 0.5 : 0
+  return fullRowCount * 10 + (10 * bonusPoints)
+}
+
+function populateBoardWithMove (board, occupiedPositions, value) {
+  _.each(occupiedPositions, function (occupiedPosition) {
+    if (value) {
+      board[occupiedPosition.x][occupiedPosition.y] = value
+    } else {
+      delete board[occupiedPosition.x][occupiedPosition.y]
+    }
+  })
+}
+
+function getMoveValue (moveNode, board) {
+  // get unique Y coords
+  let occupiedYPositions = populateToFourYCoords(moveNode.block.occupiedPositions)
+  populateBoardWithMove(board, moveNode.block.occupiedPositions, 1)
+  const reward = calculateReward(board, occupiedYPositions, _.min(occupiedYPositions))
+  populateBoardWithMove(board, moveNode.block.occupiedPositions)
+
+  return reward
+}
+
 function train () {
   const tetrisGame = new TetrisGame(3, true)
 
@@ -82,6 +167,29 @@ function train () {
     blockPositions = _.concat(newUniqueMoves, blockPositions)
   }
 
+  let bestMoves = {moveValue: -10000, sameValueMoveIndexes: []}
+
+  _.each(allMoveNodes, function (moveNode, index) {
+    if (moveNode.block.isMovable) return
+
+    let moveValue = getMoveValue(moveNode, tetrisGame.getBoard())
+
+    if (moveValue === bestMoves.moveValue) {
+      bestMoves.sameValueMoveIndexes.push(index)
+    }
+
+    if (moveValue > bestMoves.moveValue) {
+      bestMoves = {
+        moveValue,
+        sameValueMoveIndexes: [index]
+      }
+    }
+  })
+
+  const bestMoveIndex = bestMoves.sameValueMoveIndexes[_.random(_.size(bestMoves.sameValueMoveIndexes) - 1)]
+
+  // console.log(bestMoves)
+  // console.log(bestMoveIndex)
   window.GAME = tetrisGame
 }
 
